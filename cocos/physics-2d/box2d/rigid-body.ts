222f55d2fa82d60405a54805054aaeca2d71e8e6
Morteza Dastouri <dastouri70@gmail.com>
2023-02-16 16:32:38 +0330

    Revert "fix regression for box2d rigidbody not updating when node transform changes"
    This reverts commit d3a826569db998628f300f391118a66a916a48fe.

--- cocos/physics-2d/box2d/rigid-body.ts
+++ cocos/physics-2d/box2d/rigid-body.ts
@@ -82,16 +82,6 @@ export class b2RigidBody2D implements IRigidBody2D {
         this.setActive(false);
     }
 
-    _registerNodeEvents () {
-        const node = this.rigidBody.node;
-        node.on(NodeEventType.TRANSFORM_CHANGED, this.nodeTransformChanged, this);
-    }
-
-    _unregisterNodeEvents () {
-        const node = this.rigidBody.node;
-        node.off(NodeEventType.TRANSFORM_CHANGED, this.nodeTransformChanged, this);
-    }
-
     nodeTransformChanged (type) {
         if (PhysicsSystem2D.instance.stepping) {
             return;
@@ -116,8 +106,6 @@ export class b2RigidBody2D implements IRigidBody2D {
             return;
         }
 
-        this._registerNodeEvents();
-
         (PhysicsSystem2D.instance.physicsWorld as b2PhysicsWorld).addBody(this);
         this.setActive(false);
 
@@ -128,8 +116,6 @@ export class b2RigidBody2D implements IRigidBody2D {
         if (!this._inited) return;
 
         (PhysicsSystem2D.instance.physicsWorld as b2PhysicsWorld).removeBody(this);
-
-        this._unregisterNodeEvents();
 
         this._inited = false;
     }
